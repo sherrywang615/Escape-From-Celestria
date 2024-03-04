5013ca9fc2904929ca07f73ff2f27af5048cea0b
yli0402 <77415930+yli0402@users.noreply.github.com>
2024-03-03 22:56:47 -0800

    rules added

--- src/world_system.cpp
+++ src/world_system.cpp
@@ -171,7 +171,7 @@ bool WorldSystem::step(float elapsed_ms_since_last_update)
 		registry.remove_all_components_of(registry.debugComponents.entities.back());
 
 	if(renderInfo){
-		createHelpInfo(renderer, vec2(window_width_px - 220, window_height_px - 450));
+		createHelpInfo(renderer, vec2(window_width_px - 500, window_height_px - 450));
 	}
 
 	// Removing out of screen entities
