e3fd603d424c745cd73f81aabc5f0b7a74acd1ba
yiran10@student.ubc.ca <yiran10@student.ubc.ca>
2024-03-03 16:06:51 -0800

    stage commits

--- src/render_system.hpp
+++ src/render_system.hpp
@@ -49,7 +49,8 @@ class RenderSystem {
 		shader_path("Josh"),
 		shader_path("textured"),
 		shader_path("wind"),
-		shader_path("font")};
+		//shader_path("font")
+		};
 
 	std::array<GLuint, geometry_count> vertex_buffers;
 	std::array<GLuint, geometry_count> index_buffers;
