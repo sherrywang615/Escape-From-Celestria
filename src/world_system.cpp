d1a2bdc4aebae36aaa1fe57313419fba8c73c16c
peteryang76 <peteryang0704@gmail.com>
2024-03-25 22:20:17 -0700

    clean up memory leak in font

--- src/world_system.cpp
+++ src/world_system.cpp
@@ -994,7 +994,6 @@ void WorldSystem::handle_collisions()
 				{
 					Motion &motion = registry.motions.get(entity);
 					motion.velocity.y = 0;
-					Gravity &gravity = registry.gravities.get(entity);
 				}
 			}
 		}
